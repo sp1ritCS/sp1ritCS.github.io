590106d8c4cf2929020005824b2fff37678bb718
sp1ritCS <31540351+sp1ritCS@users.noreply.github.com>
2020-08-29 11:21:33 +0000

    Deploying to master from  @ 3facfebc521f925db8b9eaa92b1eef48685606b8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db1bd02eab13cc0c102daf03000c0928.js"
+  "/precache-manifest.603c2e4ddadcb1035e84bc6e862c5735.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blog"});
